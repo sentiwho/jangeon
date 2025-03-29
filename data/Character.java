721bdbe552c7ea0aae9e8b4ac965013955ef0330
monkey <plagu4@mail.ru>
2025-03-30 01:06:04 +0300

    git repo created

---
+++ data/Character.java
@@ -0,0 +1,15 @@
+public class Character {
+    int Strength;
+    int Agility;
+    int Mana;
+    int Health;
+    int RunincCircle;
+
+    public Character() {
+        this.Strength = 1;
+        this.Agility = 1;
+        this.Mana = 1;
+        this.Health = 1;
+        this.RunincCircle = 0;
+    }
+}
