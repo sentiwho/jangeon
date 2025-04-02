d9f8d834eaeb678188e1bfcf65779934792d46b3
monkey <plagu4@mail.ru>
2025-04-03 01:23:48 +0300

    Main now just call the components. StatsDistribution now a component. Added Character selection component

--- data/Character.java
+++ data/Character.java
@@ -1,21 +1,19 @@
 public class Character {
     int Strength;
     int Agility;
+    int Wisdom;
     int Mana;
     int Health;
     int RunicCircle;
  
-    public Character(int Strength, int Agility, int Mana, int Health, int RunicCircle) {
-     this.Strength = Strength;
-     this.Agility = Agility;
-     this.Mana = Mana;
-     this.Health = Health;
-     this.RunicCircle = RunicCircle;
+    public Character(int [] array) {
+     this.Strength = array[0];
+     this.Agility = array[1];
+     this.Wisdom = array[2];
+     this.Mana = array[3];
+     this.Health = array[4];
+     this.RunicCircle = array[5];
 
  }
-     public static void Character(String[] args) {
-        Character CharacterUra = new Character(1, 1, 1, 1, 0);
-        System.out.println("Now your characteristics is " + CharacterUra.Strength);
-       // Character CharacterLex = new Character(0, 0, 0, 0, 0);
-     }
+   
    }
